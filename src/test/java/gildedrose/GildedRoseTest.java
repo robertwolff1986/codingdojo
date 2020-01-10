8a2a27f12a81b97a16f9b24156115c769bb225c9
robertwolff1986 <robert.wolff1986@gmail.com>
2020-01-10 13:18:39 +0100

    gilded rose erster Anlauf

---
+++ src/test/java/gildedrose/GildedRoseTest.java
@@ -0,0 +1,20 @@
+package gildedrose;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GildedRoseTest {
+
+    @Test
+    void foo() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        Assertions.assertEquals("fixme", app.items[0].name);
+    }
+
+
+
+}
